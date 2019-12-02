1713c47ce39633cae871ead2d6d1a51d1f112d53
Audrey MARTIN <audrey.martin26@gmail.com>
2019-12-02 10:17:15 +0100

    lambda expression

--- src/test/java/java8/ex02/Lambda_02_Test.java
+++ src/test/java/java8/ex02/Lambda_02_Test.java
@@ -3,10 +3,12 @@ package java8.ex02;
 import java8.data.Account;
 import java8.data.Data;
 import java8.data.Person;
+
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 
 /**
@@ -37,7 +39,11 @@ public class Lambda_02_Test {
 
         // TODO transformer la liste de personnes en liste de comptes
         // TODO tous les objets comptes ont un solde à 100 par défaut
-        List<Account> result = map(personList, null);
+
+        PersonToAccountMapper mapper = p -> new Account(p,100);
+        	
+        
+        List<Account> result = map(personList, mapper);
 
         assert result.size() == personList.size();
         for (Account account : result) {
